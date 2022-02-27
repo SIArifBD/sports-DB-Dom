f7a9fd2488b36cdb8bad1fde26ecafe761586de9
SIArifBD <si.arif.cse@gmail.com>
2022-02-28 04:58:30 +0600

    added

--- app.js
+++ app.js
@@ -4,5 +4,29 @@ const allPlayers = () => {
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => showPlayerDetails(data.player));
+};
+
+const showPlayerDetails = (players) => {
+    console.log(players);
+    for (const player of players) {
+        const parent = document.getElementById("player-container");
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card border">
+            <div class="pro-pic">
+                <img class="w-25" src="${player.strThumb}" alt="">
+            </div>
+            <h2>Name: ${player.strPlayer}</h2>
+            <h5>Country: ${player.strNationality}</h5>
+            <p></p>
+            <div class="allbutton">
+                <div class="btn btn-danger">Delete</div>
+                <div class="btn btn-success">Details</div>
+            </div>
+        </div>
+    `;
+        parent.appendChild(div);
+    }
 };
